f532c6f73af787f75dac7cf7d6a335d8ddae585a
YongHo Shin <gdtknight@naver.com>
2023-08-02 07:19:43 +0900

    feat: [core] 연차/당직 enum 생성 (#1)

---
+++ core/src/main/java/com/example/core/model/schedule/enums/Reason.java
@@ -0,0 +1,8 @@
+package com.example.core.model.schedule.enums;
+
+public enum Reason {
+  휴가,
+  반차,
+  연차,
+  병가
+}
---
+++ core/src/main/java/com/example/core/model/schedule/enums/Status.java
@@ -0,0 +1,5 @@
+package com.example.core.model.schedule.enums;
+
+public enum Status {
+    PENDING, APPROVE, REJECT, CANCELLED
+}
