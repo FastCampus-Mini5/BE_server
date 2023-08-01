2f7f2af27dcedd2e4baecb94066690eb05e6c1f6
YongHo Shin <gdtknight@naver.com>
2023-08-02 07:36:22 +0900

    feat: [application] 시작 클래스 생성 (#1)

---
+++ application/src/main/java/com/example/application/Application.java
@@ -0,0 +1,18 @@
+package com.example.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.application", "com.example.core"})
+@EntityScan(basePackages = "com.example.core.model")
+@EnableJpaRepositories(basePackages = "com.example.core.repository")
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
