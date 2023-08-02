fb02488da6d3d0438c8d6f7edbd4dfb0cf9dc1ba
YongHo Shin <gdtknight@naver.com>
2023-08-02 14:49:10 +0900

    feat: [application] UserRequest - UpdateInfoDTO 추가 (#15)

--- application/src/main/java/com/example/application/user/dto/UserRequest.java
+++ application/src/main/java/com/example/application/user/dto/UserRequest.java
@@ -73,4 +73,17 @@ public class UserRequest {
         message = "유효하지 않은 이메일 주소입니다.")
     private String email;
   }
+
+  @Getter
+  @ToString
+  @Builder
+  public static class UpdateInfoDTO {
+    private String profileImg; // TODO : 이미지 파일 첨부
+
+    @NotBlank
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+        message = "비밀번호는 영어 대문자, 영어 소문자, 숫자, 특수문자를 모두 포함해야 하며, 최소 8글자 이상이어야 합니다.")
+    private String password;
+  }
 }
