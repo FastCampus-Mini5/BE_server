d8fe6c7ff7666a036ee4978d3523a26520e881aa
정현수 <junghss0113@gmail.com>
2023-08-02 14:28:12 +0900

    회원 가입 요청 리스트 조회 기능 구현 완료 (#28)
    * feat: [admin] 전체 회원 조회 컨트롤러 구현 (#25)
    
    * feat: [admin] 전체 회원 조회 응답 DTO 구현 (#25)
    
    * feat: [admin] 전체 회원 조회 기능 구현 (#25)
    
    * test: [admin] 전체 회원 조회 단위 테스트 코드 작성 (#25)

---
+++ admin/src/test/java/com/example/admin/user/service/UserServiceTest.java
@@ -0,0 +1,71 @@
+package com.example.admin.user.service;
+
+import com.example.admin.user.dto.UserResponse;
+import com.example.core.errors.exception.EmptyPagingDataRequestException;
+import com.example.core.model.schedule.VacationInfo;
+import com.example.core.model.user.User;
+import com.example.core.repository.schedule.VacationInfoRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private VacationInfoRepository vacationInfoRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @DisplayName("전체 유저 조회 성공 테스트")
+    @Test
+    void getAllList_Success_Test() {
+        // Given
+        List<VacationInfo> mockList = List.of(
+                VacationInfo.builder().id(1L).remainVacation(3)
+                        .user(new User()).build(),
+
+                VacationInfo.builder().id(2L).remainVacation(4)
+                        .user(new User()).build(),
+
+                VacationInfo.builder().id(3L).remainVacation(5)
+                        .user(new User()).build()
+        );
+
+        Pageable pageable = PageRequest.of(0, 3);
+        Page<VacationInfo> mockPage = new PageImpl<>(mockList, pageable, 1);
+
+        Mockito.when(vacationInfoRepository.findAll(pageable))
+                .thenReturn(mockPage);
+
+        // When
+        Page<UserResponse.ListDTO> actual = userService.getAllUsers(pageable);
+
+        // Then
+        Assertions.assertEquals(3, actual.getTotalElements());
+        Assertions.assertEquals(1, actual.getTotalPages());
+    }
+
+    @DisplayName("전체 유저 조회 실패 테스트 - 빈 페이지 정보 요청")
+    @Test
+    void getAllList_failure_Test() {
+        // Given
+        // When
+        // Then
+        Assertions.assertThrows(EmptyPagingDataRequestException.class, () -> {
+            userService.getAllUsers(null);
+        });
+    }
+}
