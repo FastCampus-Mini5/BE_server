efde0cde3ff8b1a67f5cf1c6c0e188ac67c0c78a
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-02 13:59:10 +0900

    feat: [admin] 회원 가입 승인 요청 컨트롤러 구현 (#17)

---
+++ admin/src/main/java/com/example/admin/user/controller/SignUpController.java
@@ -0,0 +1,31 @@
+package com.example.admin.user.controller;
+
+import com.example.admin.user.dto.SignUpRequest;
+import com.example.admin.user.service.SignUpService;
+import com.example.core.util.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/admin/signup")
+public class SignUpController {
+
+    private final SignUpService signUpService;
+
+    @PostMapping("/approve")
+    public ResponseEntity<ApiResponse.Result<Object>> approve(
+            @RequestBody @Valid SignUpRequest.ApproveDTO approveDTO,
+            Errors errors) {
+        log.info("POST /api/admin/signup/approve " + approveDTO);
+
+        signUpService.approve(approveDTO);
+        return ResponseEntity.ok(ApiResponse.success());
+    }
+}
