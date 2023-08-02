d8b9978cbe574d6cacb72016f1c03ba043c0d223
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-02 14:03:09 +0900

    feat: [admin] 회원 가입 승인 요청 기능 구현 (#17)

--- core/src/main/java/com/example/core/repository/user/SignUpRepository.java
+++ core/src/main/java/com/example/core/repository/user/SignUpRepository.java
@@ -4,5 +4,10 @@ import com.example.core.model.user.SignUp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface SignUpRepository extends JpaRepository<SignUp, Long> {}
+public interface SignUpRepository extends JpaRepository<SignUp, Long> {
+
+    Optional<SignUp> findByEmail(String email);
+}
