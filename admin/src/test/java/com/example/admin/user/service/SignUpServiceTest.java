b12b0d91fbc435430beed79ee15611f35c060442
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-02 14:47:55 +0900

    test: [admin] 회원 가입 리스트 조회 단위 테스트 코드 구현 (#33)

--- admin/src/test/java/com/example/admin/user/service/SignUpServiceTest.java
+++ admin/src/test/java/com/example/admin/user/service/SignUpServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.admin.user.service;
 
 import com.example.admin.user.dto.SignUpRequest;
+import com.example.admin.user.dto.SignUpResponse;
 import com.example.core.errors.exception.EmptyDtoRequestException;
+import com.example.core.errors.exception.EmptyPagingDataRequestException;
 import com.example.core.errors.exception.SignUpServiceException;
 import com.example.core.model.user.SignUp;
 import com.example.core.model.user.User;
@@ -16,6 +18,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -71,6 +81,46 @@ class SignUpServiceTest {
         // Then
         Assertions.assertThrows(SignUpServiceException.class, () -> {
             signUpService.approve(approveDTO);
+        });
+    }
+
+    @DisplayName("회원 가입 요청 리스트 조회 성공 테스트")
+    @Test
+    void getAllList_Success_Test() {
+        // Given
+        List<SignUp> mockList = List.of(
+                SignUp.builder().id(1L).email("user1@email.com").username("user1")
+                        .hireDate((Timestamp.valueOf(LocalDateTime.now()))).build(),
+
+                SignUp.builder().id(2L).email("user2@email.com").username("user2")
+                        .hireDate((Timestamp.valueOf(LocalDateTime.now()))).build(),
+
+                SignUp.builder().id(3L).email("user3@email.com").username("user3")
+                        .hireDate((Timestamp.valueOf(LocalDateTime.now()))).build()
+        );
+
+        Pageable pageable = PageRequest.of(0, 3);
+        Page<SignUp> mockPage = new PageImpl<>(mockList, pageable, 1);
+
+        Mockito.when(signUpRepository.findAll(pageable))
+                .thenReturn(mockPage);
+
+        // When
+        Page<SignUpResponse.ListDTO> actual = signUpService.getAllList(pageable);
+
+        // Then
+        Assertions.assertEquals(3, actual.getTotalElements());
+        Assertions.assertEquals(1, actual.getTotalPages());
+    }
+
+    @DisplayName("회원 가입 요청 리스트 조회 실패 테스트 - 빈 페이지 정보 요청")
+    @Test
+    void getAllList_Failure_Test() {
+        // Given
+        // When
+        // Then
+        Assertions.assertThrows(EmptyPagingDataRequestException.class, () -> {
+            signUpService.getAllList(null);
         });
     }
 }
