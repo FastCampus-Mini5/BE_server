19c52638906d0354b233b75d732b4fea08a5bb17
inyoung <charmingyear@gmail.com>
2023-08-02 14:55:17 +0900

    feat: 관리자 당직 상태별 리스트 조회 테스트 코드 구현 (#37)

--- admin/src/test/java/com/example/admin/schedule/duty/service/DutyServiceTest.java
+++ admin/src/test/java/com/example/admin/schedule/duty/service/DutyServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.admin.schedule.duty.service;
 
 import com.example.admin.schedule.duty.dto.DutyRequest;
+import com.example.admin.schedule.duty.dto.DutyResponse;
+import com.example.core.errors.exception.EmptyPagingDataRequestException;
 import com.example.core.errors.exception.ScheduleServiceException;
 import com.example.core.errors.exception.ValidStatusException;
 import com.example.core.model.schedule.Duty;
@@ -13,9 +15,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -105,5 +111,64 @@ class DutyServiceTest {
                 .createdDate(Timestamp.valueOf(LocalDateTime.now()))
                 .updatedDate(null)
                 .build();
+    }
+
+    @DisplayName("당직 목록 조회 성공")
+    @Test
+    void testGetDutyListSuccess() {
+        // given
+        String validStatus = "PENDING";
+        Pageable pageable = mock(Pageable.class);
+
+        List<User> testUsers = List.of(
+                createUser(1L, "user1"),
+                createUser(2L, "user2"),
+                createUser(3L, "user3")
+        );
+
+        List<Duty> dutyList = List.of(
+                createDuty(1L, testUsers.get(0), "2023-07-01 00:00:00"),
+                createDuty(2L, testUsers.get(1), "2023-07-03 00:00:00"),
+                createDuty(3L, testUsers.get(2), "2023-07-05 00:00:00")
+        );
+
+        Page<Duty> mockPage = new PageImpl<>(dutyList, pageable, dutyList.size());
+        when(dutyRepository.findDutyByStatus(pageable, Status.PENDING)).thenReturn(mockPage);
+
+        //when
+        Page<DutyResponse.ListDTO> result = dutyService.dutyListByStatus(pageable, validStatus);
+
+        //then
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        assertEquals(3, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
+        verify(dutyRepository, times(1)).findDutyByStatus(pageable, Status.PENDING);
+    }
+
+    @DisplayName("당직 목록 조회 실패 - 유효하지 않은 상태")
+    @Test
+    void testGetDutyListFailWithStatus() {
+        //given
+        String invalidStatus = "INVALID_STATUS";
+        Pageable pageable = mock(Pageable.class);
+
+        //when,then
+        assertThrows(ValidStatusException.class,
+                () -> dutyService.dutyListByStatus(pageable, invalidStatus));
+        verify(dutyRepository, never()).findDutyByStatus(any(), any());
+    }
+
+    @DisplayName("당직 목록 조회 실패 - 빈 페이지")
+    @Test
+    void testGetDutyListFailWithPage() {
+        //given
+        String validStatus = "APPROVED";
+        Pageable pageable = null;
+
+        //when,then
+        assertThrows(EmptyPagingDataRequestException.class,
+                () -> dutyService.dutyListByStatus(pageable, validStatus));
+        verify(dutyRepository, never()).findDutyByStatus(any(), any());
     }
 }
