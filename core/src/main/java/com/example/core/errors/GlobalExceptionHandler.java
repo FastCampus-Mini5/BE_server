d9c82621b0ae781c368e766a0dc51cb27a7016c4
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-02 12:28:47 +0900

    feat: [core] 암호화 중 발생할 수 있는 예외 처리 핸들러 구현 (#6)

--- core/src/main/java/com/example/core/errors/GlobalExceptionHandler.java
+++ core/src/main/java/com/example/core/errors/GlobalExceptionHandler.java
@@ -10,58 +10,41 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(Exception400.class)
-  public ResponseEntity<ApiResponse.Result<Object>> badRequest(Exception400 e) {
-    return new ResponseEntity<>(e.body(), e.status());
-  }
+    @ExceptionHandler(Exception400.class)
+    public ResponseEntity<ApiResponse.Result<Object>> badRequest(Exception400 e) {
+        return new ResponseEntity<>(e.body(), e.status());
+    }
 
-  @ExceptionHandler(Exception401.class)
-  public ResponseEntity<ApiResponse.Result<Object>> unAuthorized(Exception401 e) {
-    return new ResponseEntity<>(e.body(), e.status());
-  }
+    @ExceptionHandler(Exception401.class)
+    public ResponseEntity<ApiResponse.Result<Object>> unAuthorized(Exception401 e) {
+        return new ResponseEntity<>(e.body(), e.status());
+    }
 
-  @ExceptionHandler(Exception403.class)
-  public ResponseEntity<ApiResponse.Result<Object>> forbidden(Exception403 e) {
-    return new ResponseEntity<>(e.body(), e.status());
-  }
+    @ExceptionHandler(Exception403.class)
+    public ResponseEntity<ApiResponse.Result<Object>> forbidden(Exception403 e) {
+        return new ResponseEntity<>(e.body(), e.status());
+    }
 
-  @ExceptionHandler(Exception404.class)
-  public ResponseEntity<ApiResponse.Result<Object>> notFound(Exception404 e) {
-    return new ResponseEntity<>(e.body(), e.status());
-  }
+    @ExceptionHandler(Exception404.class)
+    public ResponseEntity<ApiResponse.Result<Object>> notFound(Exception404 e) {
+        return new ResponseEntity<>(e.body(), e.status());
+    }
 
-  @ExceptionHandler(Exception500.class)
-  public ResponseEntity<ApiResponse.Result<Object>> serverError(Exception500 e) {
-    return new ResponseEntity<>(e.body(), e.status());
-  }
+    @ExceptionHandler(Exception500.class)
+    public ResponseEntity<ApiResponse.Result<Object>> serverError(Exception500 e) {
+        return new ResponseEntity<>(e.body(), e.status());
+    }
 
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ApiResponse.Result<Object>> unknownServerError(Exception e) {
-    ApiResponse.Result<Object> apiResult =
-        ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse.Result<Object>> unknownServerError(Exception e) {
+        ApiResponse.Result<Object> apiResult =
+                ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-  @ExceptionHandler(EmptyPagingDataRequestException.class)
-  public ResponseEntity<ApiResponse.Result<Object>> emptyPagingDataError(
-      EmptyPagingDataRequestException e) {
-    ApiResponse.Result<Object> apiResult =
-        ApiResponse.error(e.getMessage(), HttpStatus.BAD_REQUEST);
-    return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
-  }
-
-  @ExceptionHandler(EmptyDtoRequestException.class)
-  public ResponseEntity<ApiResponse.Result<Object>> EmptyDtoRequestError(
-      EmptyDtoRequestException e) {
-    ApiResponse.Result<Object> apiResult =
-        ApiResponse.error(e.getMessage(), HttpStatus.BAD_REQUEST);
-    return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
-  }
-
-  @ExceptionHandler(SignUpServiceException.class)
-  public ResponseEntity<ApiResponse.Result<Object>> SignUpServiceError(SignUpServiceException e) {
-    ApiResponse.Result<Object> apiResult =
-        ApiResponse.error(e.getMessage(), HttpStatus.BAD_REQUEST);
-    return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
-  }
+    @ExceptionHandler({EncryptException.class, DecryptException.class})
+    public ResponseEntity<ApiResponse.Result<Object>> encryptionError(Exception e) {
+        ApiResponse.Result<Object> apiResult = ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
