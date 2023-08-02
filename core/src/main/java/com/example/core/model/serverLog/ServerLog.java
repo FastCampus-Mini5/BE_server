73aec5b25b592c0a436769a8b1a0ca46bef7dc0e
YongHo Shin <gdtknight@naver.com>
2023-08-02 17:08:44 +0900

    로그인 기록 기능 구현 완료 (#54)
    * feat: [application] ServerLogRepository 인터페이스 작성 (#46)
    
    * refactor: [application] UserController.signIn - 로그인 기록을 남기기 위해 HttpServletRequest 를 UserService 로 직접 전달 (#46)
    
    * feat: [application] UserService.signIn - 로그인 기록 메서드 추가 (#46)
    
    * refactor: ServerLog 엔티티 클래스 포맷 적용, Lombok 추가 (#46)

--- core/src/main/java/com/example/core/model/serverLog/ServerLog.java
+++ core/src/main/java/com/example/core/model/serverLog/ServerLog.java
@@ -1,26 +1,30 @@
 package com.example.core.model.serverLog;
 
 import com.example.core.model.user.User;
-import org.springframework.data.annotation.CreatedDate;
-
-import javax.persistence.*;
 import java.sql.Timestamp;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Table(name = "log_tb")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ServerLog {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
 
-    @Column(nullable = false)
-    private String requestIp;
+  @Column(nullable = false)
+  private String requestIp;
 
-    @CreatedDate
-    private Timestamp signInDate;
+  @CreatedDate private Timestamp signInDate;
 }
