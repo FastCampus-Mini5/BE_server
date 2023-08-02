24d227626ec8f400f24c06699b25c5ee9fa60209
YongHo Shin <gdtknight@naver.com>
2023-08-02 14:49:57 +0900

    feat: [application] UserResponse - UserInfoDTO 추가 (#15)

--- application/src/main/java/com/example/application/user/dto/UserResponse.java
+++ application/src/main/java/com/example/application/user/dto/UserResponse.java
@@ -11,4 +11,27 @@ public class UserResponse {
     private String email;
     private Boolean available;
   }
+
+  @Getter
+  @Builder
+  public static class UserInfoDTO {
+    private String username;
+    private String email;
+    private String profileImage;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Timestamp hireDate;
+
+    public UserInfoDTO toDecryptedDTO(Encryption encryption) {
+      String decryptedUsername = encryption.decrypt(username);
+      String decryptedEmail = encryption.decrypt(email);
+
+      return UserInfoDTO.builder()
+          .username(decryptedUsername)
+          .email(decryptedEmail)
+          .profileImage(profileImage)
+          .hireDate(hireDate)
+          .build();
+    }
+  }
 }
