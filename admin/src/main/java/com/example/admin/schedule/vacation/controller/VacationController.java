ee3731face5185ce54a36e2617cfafe13d0aa5c1
inyoung <charmingyear@gmail.com>
2023-08-02 14:54:16 +0900

    feat: 관리자 연차 상태별 리스트 조회 기능 구현 (#36)

--- admin/src/main/java/com/example/admin/schedule/vacation/controller/VacationController.java
+++ admin/src/main/java/com/example/admin/schedule/vacation/controller/VacationController.java
@@ -1,10 +1,15 @@
 package com.example.admin.schedule.vacation.controller;
 
 import com.example.admin.schedule.vacation.dto.VacationRequest;
+import com.example.admin.schedule.vacation.dto.VacationResponse;
 import com.example.admin.schedule.vacation.service.VacationService;
 import com.example.core.util.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,5 +28,13 @@ public class VacationController {
             @RequestBody @Valid VacationRequest.StatusDTO statusDTO, Error error) {
         vacationService.updateStatus(statusDTO);
         return ResponseEntity.ok(ApiResponse.success());
+    }
+
+    @GetMapping("/{status}/list")
+    public ResponseEntity<ApiResponse.Result<Page<VacationResponse.ListDTO>>> vacationListByStatus(
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+            @PathVariable(required = false) String status) {
+        Page<VacationResponse.ListDTO> vacationListPage = vacationService.vacationListByStatus(pageable, status);
+        return ResponseEntity.ok(ApiResponse.success(vacationListPage));
     }
 }
