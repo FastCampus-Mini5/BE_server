6c46a738e8573eef3576a42fcc6f14b6d5c76cbd
YongHo Shin <gdtknight@naver.com>
2023-08-02 13:10:16 +0900

    [application] 회원 가입 요청, 로그인, 중복 이메일 체크 API 구현 완료 (#14)
    * feat: [application] 회원 가입 요청, 로그인, 이메일 중복 체크를 위한 UserContoller 메서드 작성 (#5)
    
    * feat: [application] 회원 가입 요청, 로그인, 이메일 중복 체크를 위한 UserRequest (DTO) 구현 (#5)
    
    * feat: [application] 요청 응답을 위한 UserResponse (DTO) 구현 (#5)
    
    * feat: [application] 회원 가입 요청, 로그인, 이메일 중복 체크를 위한 UserService 메서드 작성 (#5)
    
    * refactor: [application] 회원 가입 요청 DTO 필드 타입 Timestamp 로 변경, Builder 패턴 적용 (#5)
    
    * test: [application] UserServiceTest 작성 (#5) - SignIn 테스트 아직 미완성 구현 필요

---
+++ application/src/main/java/com/example/application/user/dto/UserRequest.java
@@ -0,0 +1,76 @@
+package com.example.application.user.dto;
+
+import com.example.core.model.user.SignUp;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.sql.Timestamp;
+import javax.validation.constraints.*;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserRequest {
+
+  @Getter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Builder
+  @ToString
+  public static class SignUpDTO {
+
+    @NotBlank
+    @Pattern(
+        regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+        message = "유효하지 않은 이메일 형식입니다.")
+    private String email;
+
+    @NotBlank
+    @Size(min = 2, max = 15)
+    private String username;
+
+    @NotBlank
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$",
+        message = "비밀번호는 영어 대문자, 영어 소문자, 숫자, 특수문자를 모두 포함해야 하며, 최소 8글자 이상이어야 합니다.")
+    private String password;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Timestamp hireDate;
+
+    public SignUp toEntityWithHashPassword(PasswordEncoder passwordEncoder) {
+      String encodedPassword = passwordEncoder.encode(this.password);
+      return SignUp.builder()
+          .username(username) // TODO : AES 암호화
+          .password(encodedPassword)
+          .email(email) // TODO : AES 암호화
+          .hireDate(hireDate)
+          .build();
+    }
+  }
+
+  @Getter
+  @ToString
+  @Builder
+  public static class SignInDTO {
+
+    @NotBlank
+    @Pattern(
+        regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+        message = "유효하지 않은 이메일 형식입니다.")
+    private String email;
+
+    @NotBlank private String password;
+  }
+
+  @Getter
+  @ToString
+  @Builder
+  public static class CheckEmailDTO {
+    @Setter
+    @NotBlank
+    @Email(
+        regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+        message = "유효하지 않은 이메일 주소입니다.")
+    private String email;
+  }
+}
