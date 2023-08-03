c7f8fbc649286c5b3a064993abaecda7b917cdb5
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-03 16:37:36 +0900

    test: [admin] 관리자 로그인 단위 테스트 구현 (#64)

--- admin/src/test/java/com/example/admin/user/service/UserServiceTest.java
+++ admin/src/test/java/com/example/admin/user/service/UserServiceTest.java
@@ -1,11 +1,15 @@
 package com.example.admin.user.service;
 
+import com.example.admin.user.dto.UserRequest;
 import com.example.admin.user.dto.UserResponse;
 import com.example.core.config._security.encryption.Encryption;
+import com.example.core.errors.exception.AdminSignInException;
+import com.example.core.errors.exception.EmptyDtoRequestException;
 import com.example.core.errors.exception.EmptyPagingDataRequestException;
 import com.example.core.model.schedule.VacationInfo;
 import com.example.core.model.user.User;
 import com.example.core.repository.schedule.VacationInfoRepository;
+import com.example.core.repository.user.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +25,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -30,6 +35,9 @@ class UserServiceTest {
 
     @Mock
     private Encryption encryption;
+
+    @Mock
+    private UserRepository userRepository;
 
     @InjectMocks
     private UserService userService;
@@ -65,12 +73,48 @@ class UserServiceTest {
 
     @DisplayName("전체 유저 조회 실패 테스트 - 빈 페이지 정보 요청")
     @Test
-    void getAllList_failure_Test() {
+    void getAllList_Failure_Test() {
         // Given
         // When
         // Then
         Assertions.assertThrows(EmptyPagingDataRequestException.class, () -> {
             userService.getAllUsers(null);
         });
+    }
+
+    @DisplayName("관리자 로그인 실패 테스트 - 비어있는 DTO 요청")
+    @Test
+    void signIn_Failure_Test() {
+        // Given
+        UserRequest.SignInDTO signInDTO = null;
+
+        // When
+        // Then
+        Assertions.assertThrows(EmptyDtoRequestException.class, () ->
+                userService.signIn(signInDTO));
+    }
+
+    @DisplayName("관리자 로그인 실패 테스트 - 유효하지 않은 관리자 정보 요청")
+    @Test
+    void signIn_Success_Test() {
+        // Given
+        UserRequest.SignInDTO signInDTO =
+                UserRequest.SignInDTO.builder()
+                        .email("admin")
+                        .password("admin")
+                        .build();
+
+        Mockito.when(userRepository.findByEmailAndRole(ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
+                .thenReturn(Optional.empty());
+
+        Mockito.when(encryption.encrypt(ArgumentMatchers.anyString()))
+                .thenAnswer(invocation -> {
+                    String rawData = invocation.getArgument(0);
+                    return "encrypted_" + rawData;
+                });
+        // When
+        // Then
+        Assertions.assertThrows(AdminSignInException.class, () ->
+                userService.signIn(signInDTO));
     }
 }
