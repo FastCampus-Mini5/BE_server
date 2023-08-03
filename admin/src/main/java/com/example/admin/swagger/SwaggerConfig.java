27dc0b3393669129743ffe00a183391a4a19c432
inyoung <charmingyear@gmail.com>
2023-08-03 15:02:37 +0900

    docs: swagger 라이브러리 오류 수정, 3.0.0 버전 변경

--- admin/src/main/java/com/example/admin/swagger/SwaggerConfig.java
+++ admin/src/main/java/com/example/admin/swagger/SwaggerConfig.java
@@ -2,34 +2,27 @@ package com.example.admin.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
-@EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
-    private static final String SERVICE_NAME = "당연하지";
+    private static final String SERVICE_NAME = "당연하지 관리자 서버";
     private static final String API_VERSION = "V1";
     private static final String API_DESCRIPTION = "연차 당직 관리 플랫폼 당연하지";
-    private static final String API_URL = "http://localhost:8080/";
+    private static final String API_URL = "http://localhost:8081/";
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.admin"))
                 .paths(PathSelectors.any())
                 .build();
     }
@@ -39,19 +32,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .version(API_VERSION)
                 .description(API_DESCRIPTION)
                 .termsOfServiceUrl(API_URL)
-                .licenseUrl("라이센스 표시할 url")
                 .build();
 
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        // -- Static resources
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
