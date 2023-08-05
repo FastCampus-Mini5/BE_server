3b5391c0ae0596b5b71f66b15cb7d4eae52841a8
정현수 <junghss0113@gmail.com>
2023-08-05 22:42:47 +0900

    관리자 유저, 회원가입 도메인 통합 테스트 구현 (#95)
    * fix: [admin] 키 값 변경으로 인해 암호화 기능에서 발생하는 오류 해결 (#89)
    
    * test: [admin] 전체 회원 정보 조회 통합 테스트 구현 (#88)
    
    * test: [admin] 회원 가입 승인 통합 테스트 구현 (#88)
    
    * test: [admin] 회원 가입 요청 리스트 조회 통합 테스트 구현 (#88)
    
    * chore: [admin] 통합 테스트를 jwt 라이브러리 의존성 추가 (#88)
    
    * test: [admin] 관리자 로그인 통합 테스트 구현 (#88)

---
+++ admin/src/test/java/com/example/admin/user/SignUpIntegrationTest.java
@@ -0,0 +1,121 @@
+package com.example.admin.user;
+
+import com.example.admin.user.dto.SignUpRequest;
+import com.example.admin.user.dto.SignUpResponse;
+import com.example.admin.user.service.SignUpService;
+import com.example.core.config._security.encryption.Encryption;
+import com.example.core.errors.ErrorMessage;
+import com.example.core.errors.exception.EmptyDtoRequestException;
+import com.example.core.errors.exception.EmptyPagingDataRequestException;
+import com.example.core.errors.exception.SignUpServiceException;
+import com.example.core.model.user.SignUp;
+import com.example.core.model.user.User;
+import com.example.core.repository.user.SignUpRepository;
+import com.example.core.repository.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Optional;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+public class SignUpIntegrationTest {
+
+    @Autowired
+    private SignUpService signUpService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SignUpRepository signUpRepository;
+
+    @Autowired
+    private Encryption encryption;
+
+    @DisplayName("회원 가입 요청 승인 테스트 - 성공")
+    @Test
+    void approve_Success_Test() {
+        // Given
+        final String email = "test7@test.com";
+        final String encryptedEmail = encryption.encrypt(email);
+
+        SignUpRequest.ApproveDTO approveDTO =
+                new SignUpRequest.ApproveDTO(email);
+
+        // When
+        signUpService.approve(approveDTO);
+        Optional<SignUp> signUpOptional = signUpRepository.findByEmail(encryptedEmail);
+        Optional<User> userOptional = userRepository.findByEmail(encryptedEmail);
+
+        // Then
+        Assertions.assertTrue(signUpOptional.isEmpty());
+        Assertions.assertTrue(userOptional.isPresent());
+    }
+
+    @DisplayName("회원 가입 요청 승인 테스트 - 실패 [유효하지 않은 Email 요청]")
+    @Test
+    void approve_Failure_Test_GivenInvalidEmail() {
+        // Given
+        SignUpRequest.ApproveDTO approveDTO =
+                new SignUpRequest.ApproveDTO("invalid@email.com");
+
+        // When
+        // Then
+        Assertions.assertThrows(SignUpServiceException.class, () ->
+                signUpService.approve(approveDTO));
+
+        try {
+            signUpService.approve(approveDTO);
+        } catch (SignUpServiceException exception) {
+            Assertions.assertEquals(ErrorMessage.NOT_FOUND_SIGNUP, exception.getMessage());
+        }
+    }
+
+    @DisplayName("회원 가입 요청 승인 테스트 - 실패 [비어있는 DTO 요청]")
+    @Test
+    void approve_Failure_Test_GivenEmptyDTO() {
+        // Given
+        SignUpRequest.ApproveDTO approveDTO = null;
+
+        // When
+        // Then
+        Assertions.assertThrows(EmptyDtoRequestException.class, () ->
+                signUpService.approve(approveDTO));
+    }
+
+    @DisplayName("회원 가입 요청 리스트 조회 테스트 - 성공")
+    @Test
+    void getAllList_Success_Test() {
+        // Given
+        Pageable pageable = PageRequest.of(1, 4);
+
+        // When
+        Page<SignUpResponse.ListDTO> actual = signUpService.getAllList(pageable);
+
+        // Then
+        Assertions.assertEquals(4, actual.getSize());
+        Assertions.assertEquals(6, actual.getTotalElements());
+        Assertions.assertEquals("senior", actual.getContent().get(0).getUsername());
+        Assertions.assertEquals("junior", actual.getContent().get(1).getUsername());
+    }
+
+    @DisplayName("회원 가입 요청 리스트 조회 테스트 - 실패 [빈 페이징 데이터 전달]")
+    @Test
+    void getAllList_Failure_Test_GivenEmptyPageableData() {
+        // Given
+        Pageable pageable = null;
+
+        // When
+        // Then
+        Assertions.assertThrows(EmptyPagingDataRequestException.class, () ->
+                signUpService.getAllList(pageable));
+    }
+}
---
+++ admin/src/test/java/com/example/admin/user/UserIntegrationTest.java
@@ -0,0 +1,95 @@
+package com.example.admin.user;
+
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.admin.user.dto.UserRequest;
+import com.example.admin.user.dto.UserResponse;
+import com.example.admin.user.service.UserService;
+import com.example.core.config._security.jwt.JwtTokenProvider;
+import com.example.core.errors.exception.EmptyDtoRequestException;
+import com.example.core.errors.exception.EmptyPagingDataRequestException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+public class UserIntegrationTest {
+
+    @Autowired
+    private UserService userService;
+
+    @DisplayName("전체 유저 정보 조회 통합 테스트 - 성공")
+    @Test
+    void getAllList_Success_Test() {
+        // Given
+        Pageable pageable = PageRequest.of(1, 4);
+
+        // When
+        Page<UserResponse.ListDTO> actual = userService.getAllUsers(pageable);
+
+        // Then
+        Assertions.assertEquals(4, actual.getSize());
+        Assertions.assertEquals("senior", actual.getContent().get(0).getUsername());
+        Assertions.assertEquals("junior", actual.getContent().get(1).getUsername());
+    }
+
+    @DisplayName("전체 유저 정보 조회 통합 테스트 - 실패 [빈 페이징 데이터 전달]")
+    @Test
+    void getAllList_Failure_Test_GivenEmptyPageableData() {
+        // Given
+        Pageable pageable = null;
+
+        // When
+        // Then
+        Assertions.assertThrows(EmptyPagingDataRequestException.class, () ->
+                userService.getAllUsers(pageable));
+    }
+
+    @DisplayName("관리자 로그인 테스트 - 성공 [토큰 생성 및 검증]")
+    @Test
+    void signIn_Success_Test() {
+        // Given
+        UserRequest.SignInDTO signInDTO =
+                new UserRequest.SignInDTO("admin", "admin");
+
+        // When
+        UserResponse.SignInDTO response = userService.signIn(signInDTO);
+        String token = response.getJwt();
+        String jwt = token.replace(JwtTokenProvider.TOKEN_PREFIX, "");
+        DecodedJWT decodedJWT = JwtTokenProvider.verify(jwt);
+
+        // Then
+        Assertions.assertEquals("ADMIN", decodedJWT.getClaim("role").asString());
+    }
+
+    @DisplayName("관리자 로그인 테스트 - 실패 [비어있는 DTO 전달]")
+    @Test
+    void signIn_Failure_Test_GivenEmptyDTO() {
+        // Given
+        UserRequest.SignInDTO signInDTO = null;
+
+        // When
+        // Then
+        Assertions.assertThrows(EmptyDtoRequestException.class, () ->
+                userService.signIn(signInDTO));
+    }
+
+    @DisplayName("관리자 로그인 테스트 - 실패 [유효하지 않은 관리자 정보 전달]")
+    @Test
+    void signIn_Failure_Test_GivenInvalidSignInData() {
+        // Given
+        UserRequest.SignInDTO signInDTO =
+                new UserRequest.SignInDTO("admin", "invalidPassword");
+        // When
+        // Then
+        Assertions.assertThrows(BadCredentialsException.class, () ->
+                userService.signIn(signInDTO));
+    }
+}
