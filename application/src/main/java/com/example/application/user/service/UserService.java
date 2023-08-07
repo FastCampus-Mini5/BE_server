fa07dffb17492aaf68c1b52b8dbcfabfa819b942
YongHo Shin <gdtknight@naver.com>
2023-08-07 15:06:26 +0900

    refactor: [application] UserService - 로깅 서비스 분리를 위한 리팩토링 (#111)

--- application/src/main/java/com/example/application/user/service/UserService.java
+++ application/src/main/java/com/example/application/user/service/UserService.java
@@ -12,16 +12,11 @@ import com.example.core.errors.exception.Exception500;
 import com.example.core.errors.exception.MailSendFailureException;
 import com.example.core.errors.exception.UserNotFoundException;
 import com.example.core.model.schedule.VacationInfo;
-import com.example.core.model.serverLog.ServerLog;
 import com.example.core.model.user.User;
 import com.example.core.repository.schedule.VacationInfoRepository;
-import com.example.core.repository.serverLog.ServerLogRepository;
 import com.example.core.repository.user.SignUpRepository;
 import com.example.core.repository.user.UserRepository;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.UUID;
-import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -41,7 +36,6 @@ public class UserService {
   private final SignUpRepository signUpRepository;
   private final UserRepository userRepository;
   private final VacationInfoRepository vacationInfoRepository;
-  private final ServerLogRepository serverLogRepository;
   private final Encryption encryption;
   private final TempPasswordMailSender mailSender;
 
@@ -51,10 +45,8 @@ public class UserService {
     signUpRepository.save(signUpDTO.toEntityEncrypted(passwordEncoder, encryption));
   }
 
-  public String signIn(HttpServletRequest request, UserRequest.SignInDTO signInDTO) {
+  public String signIn(UserRequest.SignInDTO signInDTO) {
     if (signInDTO == null) throw new Exception500(ErrorMessage.EMPTY_DATA_TO_SIGNIN);
-
-    String clientIP = getClientIP(request);
 
     UsernamePasswordAuthenticationToken token =
         new UsernamePasswordAuthenticationToken(
@@ -66,44 +58,7 @@ public class UserService {
 
     final User user = userDetail.getUser();
 
-    logging(user, clientIP);
-
     return JwtTokenProvider.create(user);
-  }
-
-  private String getClientIP(HttpServletRequest request) {
-    String[] headerNames = {
-      "X-Forwarded-For",
-      "Proxy-Client-IP",
-      "WL-Proxy-Client-IP",
-      "HTTP_CLIENT_IP",
-      "HTTP_X_FORWARDED_FOR"
-    };
-
-    for (String header : headerNames) {
-      String ipAddress = request.getHeader(header);
-      if (ipAddress != null && !ipAddress.isEmpty() && !"unknown".equalsIgnoreCase(ipAddress)) {
-        // 일부 프록시들은 쉼표로 구분된 IP 주소 목록을 사용하며, 실제 클라이언트 IP는 첫 번째로 옵니다.
-        if (ipAddress.contains(",")) {
-          return ipAddress.split(",")[0].trim();
-        }
-        return ipAddress;
-      }
-    }
-
-    return request.getRemoteAddr();
-  }
-
-  private void logging(User user, String clientIp) {
-
-    ServerLog serverLog =
-        ServerLog.builder()
-            .user(user)
-            .requestIp(clientIp)
-            .signInDate(Timestamp.valueOf(LocalDateTime.now()))
-            .build();
-
-    serverLogRepository.save(serverLog);
   }
 
   public UserResponse.AvailableEmailDTO checkEmail(UserRequest.CheckEmailDTO checkEmailDTO) {
