d8c63368d5b0ba49240872aa1f7e85d6c7ef3304
YongHo Shin <gdtknight@naver.com>
2023-08-07 22:23:55 +0900

    fix: [application] UserRequest - Validation Message 하드코딩 제거 (#100)

--- application/src/main/java/com/example/application/user/dto/UserRequest.java
+++ application/src/main/java/com/example/application/user/dto/UserRequest.java
@@ -1,8 +1,5 @@
 package com.example.application.user.dto;
 
-import static com.example.core.errors.ErrorMessage.INVALID_EMAIL;
-import static com.example.core.errors.ErrorMessage.INVALID_PASSWORD;
-
 import com.example.core.config._security.encryption.Encryption;
 import com.example.core.model.user.SignUp;
 import com.fasterxml.jackson.annotation.JsonFormat;
