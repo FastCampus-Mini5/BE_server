e64f165b5c3f4c1cbacd7e6dd7a7b475b6864ba0
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-07 13:43:24 +0900

    fix: [admin] application.yml의 설정 변경 없이도 test 환경설정에 따라 동작하도록 수정 (#105)

--- admin/src/test/java/com/example/admin/user/SignUpIntegrationTest.java
+++ admin/src/test/java/com/example/admin/user/SignUpIntegrationTest.java
@@ -20,12 +20,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.yml")
+@ActiveProfiles("test")
 public class SignUpIntegrationTest {
 
     @Autowired
--- admin/src/test/java/com/example/admin/user/UserIntegrationTest.java
+++ admin/src/test/java/com/example/admin/user/UserIntegrationTest.java
@@ -16,10 +16,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.yml")
+@ActiveProfiles("test")
 public class UserIntegrationTest {
 
     @Autowired
