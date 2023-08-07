4c8a854561500609a9f540a2576ec035a2fa7799
YongHo Shin <gdtknight@naver.com>
2023-08-08 01:12:59 +0900

    [application] 중복 이메일로 회원 가입 요청이 들어오는 경우 에러 처리 추가 (#120)
    * refactor: ErrorMessage - 에러메세지 명확하게 수정, 포맷팅 적용, 중복 이메일 관련 메세지 추가 (#119)
    
    * refactor: UserService - 중복 이메일로 가입 요청시 에외 처리 추가 및 @Transactional 적용 (#119)

--- core/src/main/java/com/example/core/errors/ErrorMessage.java
+++ core/src/main/java/com/example/core/errors/ErrorMessage.java
@@ -6,12 +6,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ErrorMessage {
   public static final String EMPTY_DATA_TO_SIGNUP = "회원 가입을 위한 데이터가 존재하지 않습니다.";
-  public static final String EMPTY_DATA_TO_SIGNIN= "로그인을 위한 데이터가 존재하지 않습니다.";
+  public static final String EMPTY_DATA_TO_SIGNIN = "로그인을 위한 데이터가 존재하지 않습니다.";
   public static final String EMPTY_DATA_TO_CHECK_EMAIL = "중복 확인을 위한 이메일이 존재하지 않습니다.";
   public static final String EMPTY_DATA_TO_FIND_PASSWORD = "비밀번호 찾기를 위한 데이터가 존재하지 않습니다.";
 
-  public static final String UN_AUTHORIZED = "UNAUTH";
-  public static final String FORBIDDEN = "FORBIDDEN";
+  public static final String UN_AUTHORIZED = "인증되지 않았습니다.";
+  public static final String FORBIDDEN = "접근이 거부되었습니다.";
 
   public static final String LOGIN_FAILED = "회원 정보가 존재하지 않습니다.";
   public static final String USER_NOT_FOUND = "사용자를 찾을 수 없습니다.";
@@ -32,9 +32,9 @@ public class ErrorMessage {
   public static final String ENCRYPT_ERROR = "암호화 오류: ";
   public static final String DECRYPT_ERROR = "복호화 오류: ";
 
-  public static final String TOKEN_UN_AUTHORIZED = "UNAUTHORIZED";
-  public static final String TOKEN_EXPIRED= "EXPIRED";
-  public static final String TOKEN_VERIFICATION_FAILED= "VERIFICATION_FAILED";
+  public static final String TOKEN_UN_AUTHORIZED = "토큰 인증에 실패하였습니다.";
+  public static final String TOKEN_EXPIRED = "토큰이 만료되었습니다.";
+  public static final String TOKEN_VERIFICATION_FAILED = "올바른 토큰이 아닙니다.";
 
   public static final String EMPTY_DATA_TO_REQUEST_VACATION = "연차 신청 정보가 없습니다.";
   public static final String VACATION_INFO_NOT_FOUND = "연차 정보를 찾을 수 없습니다.";
@@ -52,6 +52,9 @@ public class ErrorMessage {
 
   public static final String EMAIL_SEND_FAILED = "이메일 전송 중 오류가 발생했습니다.";
   public static final String INVALID_EMAIL = "유효하지 않은 이메일 주소입니다.";
-  public static final String INVALID_PASSWORD = "비밀번호는 영어 대문자, 영어 소문자, 숫자, 특수문자를 모두 포함해야 하며, 최소 8글자 이상이어야 합니다.";
-    public static final String INVALID_ADMIN_EMAIL = "유효하지 않은 관리자 정보입니다.";
+  public static final String INVALID_PASSWORD =
+      "영어 대문자, 소문자, 숫자, 특수문자 모두 포함. 최소 8글자 이상.";
+  public static final String INVALID_ADMIN_EMAIL = "유효하지 않은 관리자 정보입니다.";
+
+  public static final String DUPLICATED_EMAIL = "해당 이메일의 회원 가입 요청이 존재합니다.";
 }
