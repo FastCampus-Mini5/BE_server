9468123cfee3ab071ec7d6e144ada912f9bf762c
YongHo Shin <gdtknight@naver.com>
2023-08-08 01:21:22 +0900

    feat: SignUpRepository - 중복 이메일 회원 가입 요청시 예외 처리와 관련된 메서드 추가 (#119, #121)

--- core/src/main/java/com/example/core/repository/user/SignUpRepository.java
+++ core/src/main/java/com/example/core/repository/user/SignUpRepository.java
@@ -1,13 +1,14 @@
 package com.example.core.repository.user;
 
 import com.example.core.model.user.SignUp;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface SignUpRepository extends JpaRepository<SignUp, Long> {
 
-    Optional<SignUp> findByEmail(String email);
+  Optional<SignUp> findByEmail(String email);
+
+  Boolean existsByEmail(String email);
 }
