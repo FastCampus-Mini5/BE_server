1fed946c6b246670191761e1c6ecdd25c6e1af88
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-08-08 15:58:17 +0900

    chore: 애플리케이션 런타임에 스케줄러가 동작하도록 설정 (#129)

--- application/src/main/java/com/example/application/Application.java
+++ application/src/main/java/com/example/application/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.application", "com.example.core"})
 @EntityScan(basePackages = "com.example.core.model")
 @EnableJpaRepositories(basePackages = "com.example.core.repository")
+@EnableScheduling
 public class Application {
 
   public static void main(String[] args) {
