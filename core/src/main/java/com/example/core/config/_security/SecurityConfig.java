70f6ce9ceb6cb017d6386368b6947dfe252c69ae
YongHo Shin <gdtknight@naver.com>
2023-08-09 21:15:20 +0900

    fix: [core] 회원 가입 요청 인증 생략하도록 경로 추가 (#161)

--- core/src/main/java/com/example/core/config/_security/SecurityConfig.java
+++ core/src/main/java/com/example/core/config/_security/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                     .antMatchers(
                         "/api/admin/user/signIn",
                         "/api/user/signin",
+                        "/api/user/signup",
                         "/api/user/findPassword",
                         "/api/user/emailCheck")
                     .permitAll()
